bfddaee206f1eb710e59badccfb1814696b22e04
Luci <luci@localhost.localdomain>
2024-07-28 19:37:12 -0300

    añadido el submodulo baton, espero que funcione

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- SPDX-FileCopyrightText: 2024 dpkgluci
+--
+-- SPDX-License-Identifier: MIT
+
+-- this game uses baton as a library for input
+-- documentation at https://github.com/tesselode/baton
+baton = require 'lib/baton/baton' -- if your baton.lua is in the root directory
