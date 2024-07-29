b15c8629c78439a8caa10f18923d491a9a4150c3
Luci <luci@localhost.localdomain>
2024-07-28 22:17:46 -0300

    starting to add a map

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ SPDX-License-Identifier: MIT
 
 # platalov1
 A platformer written in lua. This could mutate into something different later on
+
+if you want to clone this project you have to run
+```
+git clone --recursive https://github.com/germe-deb/platalov1/tree/main
+```
--- main.lua
+++ main.lua
@@ -2,6 +2,60 @@
 --
 -- SPDX-License-Identifier: MIT
 
--- this game uses baton as a library for input
--- documentation at https://github.com/tesselode/baton
-baton = require 'lib/baton/baton' -- if your baton.lua is in the root directory
+-- libraries:
+-- https://github.com/tesselode/baton input management
+local baton = require 'lib/baton/baton'
+-- https://github.com/tesselode/baton collision and reaction
+local bump = require 'lib/bump/bump'
+-- https://github.com/kikito/anim8 sprite animations
+local anim8 = require 'lib/anim8/anim8'
+
+-- key mapping (usando baton)
+local player = baton.new {
+    controls = {
+        left = {'key:left', 'button:dpleft', 'axis:leftx-'},
+        right = {'key:right', 'button:dpright', 'axis:leftx+'},
+        up = {'key:up', 'button:dpup', 'axis:lefty-'},
+        down = {'key:down', 'button:dpdown', 'axis:lefty+'},
+
+        action = {'key:t', 'button:b'},
+    },
+    pairs = {
+        move = {"left", "right", "up", "down"}
+    },
+    joystick = love.joystick.getJoysticks()[1],
+}
+
+
+-- variables
+local px, py = 0, 0
+
+-- aca empieza el codigo
+
+function love.load()
+    
+end
+
+function love.update(dt)
+    player:update()
+
+    -- local x, y = input:get 'move'
+    -- playership:move(x*100, y*100)
+    if player:down 'left' then
+        px = px - 10
+    end
+    if player:down 'right' then
+        px = px + 10
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", px, py, 50, 50)
+
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
